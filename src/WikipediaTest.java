8aa69dfac9aea7f13279de03a22b3ad1589c7310
Tatiana <tanya00pm@yandex.ru>
2021-09-03 00:09:36 +0200

    lesson 3 / task 2

--- src/WikipediaTest.java
+++ src/WikipediaTest.java
@@ -39,12 +39,71 @@ public class WikipediaTest {
         driver.quit();
     }
 
-    @Test
+    /*@Test
     public void testCompareSearchFieldText()
     {
         Assert.assertTrue(assertElementHasText(By.id("org.wikipedia:id/search_container"),
                 "Search Wikipedia",
                 "Web element does not contain the desired text."));
+    }*/
+
+    @Test
+    public void testCancelSearch()
+    {
+        // set focus on the search field
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Can not find Wikipedia input",
+                5
+        );
+
+        // search for the text "Kotlin"
+        waitForElementAndSendKeys(By.xpath("//*[contains(@text, 'Search…')]"), "Kotlin",
+                "Can not find search input", 5);
+
+        // find the search result container and its instances number by image previews (for each instance it is unique)
+        int instance_number = getElementInstanceNumber(By.id("org.wikipedia:id/search_results_list"));
+        Assert.assertTrue("No result instances found", (instance_number > 0));
+
+        // clear search field
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Can not find search field", 5
+        );
+
+        // make sure that search field is empty (has a default text)
+        Assert.assertTrue("Search field is not empty:",
+                assertElementHasDefaultText(By.id("org.wikipedia:id/search_src_text"),
+                        "Search…",
+                        "Web element does not contain the default text."));
+
+        // make sure that no search result container presents anymore
+        waitForElementNotPresent(
+                By.id("org.wikipedia:id/search_results_list"),
+                "Result list is still present on the page",
+                5
+        );
+    }
+
+    private WebElement waitForElementAndClick(By by, String error_message, long timeOutInSeconds)
+    {
+        WebElement element = waitForElementPresent(by, error_message, timeOutInSeconds);
+        element.click();
+        return element;
+    }
+
+    private WebElement waitForElementAndSendKeys(By by, String value, String error_message, long timeOutInSeconds)
+    {
+        WebElement element = waitForElementPresent(by, error_message, timeOutInSeconds);
+        element.sendKeys(value);
+        return element;
+    }
+
+    private WebElement waitForElementAndClear(By by, String error_message, long timeOutInSeconds)
+    {
+        WebElement element = waitForElementPresent(by, error_message, timeOutInSeconds);
+        element.clear();
+        return element;
     }
 
     private boolean assertElementHasText(By by, String text, String error_message)
@@ -66,6 +125,36 @@ public class WikipediaTest {
         return false;
     }
 
+    private boolean assertElementHasDefaultText(By by, String default_text, String error_message)
+    {
+        WebElement search_field_element = waitForElementPresent(
+                by,
+                "Can not find searched field",
+                5
+        );
+
+        if(search_field_element.getAttribute("text").equals(default_text))
+            return true;
+
+        System.out.println(error_message + "\nExpected result: '" + default_text
+                + "'\nActual result: '"+ search_field_element.getAttribute("text")+"");
+
+        return false;
+    }
+
+    private int getElementInstanceNumber(By by)
+    {
+        WebElement container_element = waitForElementPresent(
+                by,
+                "Can not find searched field",
+                5
+        );
+
+        int number = container_element.findElements(By.className("android.widget.ImageView")).size();
+
+        return number;
+    }
+
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds)
     {
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
@@ -73,6 +162,16 @@ public class WikipediaTest {
 
         return wait.until(
                 ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    private boolean waitForElementNotPresent(By by, String error_message, long timeOutInSeconds)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+        wait.withMessage(error_message+ "\n");
+
+        return wait.until(
+                ExpectedConditions.invisibilityOfElementLocated(by)
         );
     }
 }
